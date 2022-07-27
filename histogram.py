e04bbfc47d857787b82ea5c0a4d5faefd2787df2
Ryan Kim <ryankim@north-10-21-18-66.wireless.griffith.edu.au>
2022-07-27 14:18:04 +1000

    Histogram updated

--- histogram.py
+++ histogram.py
@@ -1,11 +1,11 @@
-Python 3.9.2 (v3.9.2:1a79785e3e, Feb 19 2021, 09:06:10) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>>
+# Python 3.9.2 (v3.9.2:1a79785e3e, Feb 19 2021, 09:06:10)
+# [Clang 6.0 (clang-600.0.57)] on darwin
+# Type "help", "copyright", "credits" or "license()" for more information.
+# >>>
 import sys
 import string
 
-lines = sys.stadin.readlines()
+lines = sys.stdin.readlines()
 total = 0
 for line in lines:
     line = line.split()
@@ -18,8 +18,8 @@ for line in lines:
     line = line.split()
     word = line[0]
     count = int(line[1])
-    per = int(count/total)*100
+    per = (count/total)*100
 
     print(word,'\t\t[', end='')
-    print(((per+4)//5)*"*", end='')
-    print('] ', per, "%", sep='')
+    print(int(((per+4)//5))*"*", end='')
+    print('] ', per // 1, "%", sep='')
